3e215f8a581c9ecd83470398a333ff5c08e8fde5
johnr46 <machine.the@hotmail.com>
2019-09-08 23:43:11 +0700

    Step 1 create project

---
+++ src/test/java/com/javaverse/course/springninja/SpringNinjaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javaverse.course.springninja;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringNinjaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
